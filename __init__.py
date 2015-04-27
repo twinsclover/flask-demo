90ec53ed01776a1dcb0d45cde28765b16fb3ed70
twinsclover <twinsclover@gmail.com>
2015-04-26 23:30:13 -0700

    Remove unnecessary file.

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
-
-app = Flask(__name__)
-app.config.from_object('peas.config')
-
-from peas import views
